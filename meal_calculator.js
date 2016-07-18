96f3880765986ad75253734a3eff82cc80569d0b
David Conner <agilebluefox@gmail.com>
2016-07-18 13:59:11 -0400

    Refactored the code to use class, let, and some arrow functions.

--- meal_calculator.js
+++ meal_calculator.js
@@ -7,145 +7,157 @@ const tax_rate = 0.09;
 const tip_percent = 0.20;
 
 // Constructor for food item
-var MenuItem = function (dish, cost) {
-    this.dish = dish;
-    this.cost = cost;
+class MenuItem {
+    constructor(dish, cost) {
+        this.dish = dish;
+        this.cost = cost;
+    }
+
 };
 
 // The food
-var pizza = new MenuItem('Pizza Slice', 8.99);
-var pasta = new MenuItem('Spaghetti', 11.99);
-var salad = new MenuItem('Cobb Salad', 11.99);
-var salmon = new MenuItem('Blackened Salmon', 14.99);
-var soup = new MenuItem('Sausage and Lentil', 5.99);
-var hamburger = new MenuItem('Hamburger', 7.99);
-var vegetable_mix = new MenuItem('Mixed Vegetables', 3.99);
-var bread = new MenuItem('Garlic Bread', 3.99);
-var beer = new MenuItem('Guiness', 6.50);
-var water = new MenuItem('Pellegrino Mineral Water', 2.99);
-var tea = new MenuItem('Iced Tea', 2.75);
-var milk = new MenuItem('Milk', 3.50);
+let pizza = new MenuItem('Pizza Slice', 8.99);
+let pasta = new MenuItem('Spaghetti', 11.99);
+let salad = new MenuItem('Cobb Salad', 11.99);
+let salmon = new MenuItem('Blackened Salmon', 14.99);
+let soup = new MenuItem('Sausage and Lentil', 5.99);
+let hamburger = new MenuItem('Hamburger', 7.99);
+let vegetable_mix = new MenuItem('Mixed Vegetables', 3.99);
+let bread = new MenuItem('Garlic Bread', 3.99);
+let beer = new MenuItem('Guiness', 6.50);
+let water = new MenuItem('Pellegrino Mineral Water', 2.99);
+let tea = new MenuItem('Iced Tea', 2.75);
+let milk = new MenuItem('Milk', 3.50);
 
 // Constructor for diner objects
-var Diner = function (name) {
-    this.name = name;
-    this.meal = [];
-    this.mealSubtotal = 0;
-    this.mealTax = 0;
-    this.mealTip = 0;
-    this.mealTotal = 0;
-};
+class Diner {
+    constructor(name) {
+        this.name = name;
+        this.meal = [];
+        this.mealSubtotal = 0;
+        this.mealTax = 0;
+        this.mealTip = 0;
+        this.mealTotal = 0;
+    }
 
-// Add items to the diner's meal
-Diner.prototype.addItem = function () {
-    let item = Array.prototype.slice.call(arguments, 0);
-    item.forEach((food, index, item) => {
-        this.meal.push(food);
-        this.mealSubtotal += food.cost;
-    });
-    return;
+    // Add items to the diner's meal
+    addItem() {
+        let item = Array.prototype.slice.call(arguments, 0);
+        item.forEach((food, index, item) => {
+            this.meal.push(food);
+            this.mealSubtotal += food.cost;
+        });
+        return;
+    }
+
 };
 
 // Constructor for the bill
-var Bill = function () {
-    this.diners = Array.prototype.slice.call(arguments, 0);
-    this.subtotal = 0;
-    this.tax = 0;
-    this.tip = 0;
-    this.total = 0;
-};
+class Bill {
+    constructor() {
+        this.diners = Array.prototype.slice.call(arguments, 0);
+        this.subtotal = 0;
+        this.tax = 0;
+        this.tip = 0;
+        this.total = 0;
+    }
 
-// Calculate the total cost of all the food consumed by the diners.
-Bill.prototype.calcSubtotal = function () {
-    let subtotal = 0;
-    let listOfItems = (item, index, meal) => {
+    // Calculate the total cost of all the food consumed by the diners.
+    calcSubtotal() {
+        let subtotal = 0;
+        let listOfItems = (item, index, meal) => {
             logItems(item.dish, item.cost);
             subtotal += item.cost;
         };
-    for (let i = 0; i < this.diners.length; i++) {
-        let meal = this.diners[i].meal;
-        meal.forEach(listOfItems);
+        for (let i = 0; i < this.diners.length; i++) {
+            let meal = this.diners[i].meal;
+            meal.forEach(listOfItems);
+        }
+        logSubtotal(subtotal);
+        return subtotal;
     }
-    logSubtotal(subtotal);
-    return subtotal;
-};
 
-// Calculate the total tip
-Bill.prototype.totalTip = function (totalFood) {
-    let tip = (Math.round((totalFood * tip_percent)* 100)) / 100;
-    logTip(tip);
-    return tip;
-};
-
-Bill.prototype.totalTax = function (totalFood) {
-    let tax = (Math.round((totalFood * tax_rate) * 100)) / 100;
-    logTax(tax);
-    return tax;
-};
-
-// Method to calculate the bill for each diner
-Bill.prototype.itemizeBill = function () {
-
-    // Get the subtotal for each diner
-    let listOfDiners = (diner, index, diners) => {
-        logDinerBill(diner.name);
-
-        diner.meal.forEach(listOfItems);
-        logSubtotal(diner.mealSubtotal);
-
-        diner.mealTax = (Math.round((diner.mealSubtotal * tax_rate) * 100)) / 100;
-        logTax(diner.mealTax);
-
-        diner.mealTip = (Math.round(this.tip * 100) / this.diners.length) / 100;
-        logTip(diner.mealTip);
-
-        diner.mealTotal = diner.mealSubtotal + diner.mealTax + diner.mealTip;
-        logTotal(diner.mealTotal);
-    }
-    let listOfItems = (item, index, meal) => {
-        logItems(item.dish, item.cost);
+    // Calculate the total tip
+    totalTip(totalFood) {
+        let tip = (Math.round((totalFood * tip_percent) * 100)) / 100;
+        logTip(tip);
+        return tip;
     };
-    this.diners.forEach(listOfDiners);
+
+    totalTax(totalFood) {
+        let tax = (Math.round((totalFood * tax_rate) * 100)) / 100;
+        logTax(tax);
+        return tax;
+    };
+
+    // Method to calculate the bill for each diner
+    itemizeBill() {
+
+        // Get the subtotal for each diner
+        let listOfDiners = (diner, index, diners) => {
+            logDinerBill(diner.name);
+
+            diner.meal.forEach(listOfItems);
+            logSubtotal(diner.mealSubtotal);
+
+            diner.mealTax = (Math.round((diner.mealSubtotal * tax_rate) *
+                100)) / 100;
+            logTax(diner.mealTax);
+
+            diner.mealTip = (Math.round(this.tip * 100) / this.diners.length) /
+                100;
+            logTip(diner.mealTip);
+
+            diner.mealTotal = diner.mealSubtotal + diner.mealTax +
+                diner.mealTip;
+            logTotal(diner.mealTotal);
+        }
+        let listOfItems = (item, index, meal) => {
+            logItems(item.dish, item.cost);
+        };
+        this.diners.forEach(listOfDiners);
+    };
+
 };
 
 // Print the item and cost to the console
-function logItems (dish, cost) {
+function logItems(dish, cost) {
     console.log(dish + ': $' + cost.toFixed(2))
 }
 
 // Print the subtotal to the console
-function logSubtotal (subtotal) {
+function logSubtotal(subtotal) {
     console.log('---------------\nSubtotal: $' + subtotal.toFixed(2));
 }
 
 // Print the tip amount to the console
-function logTip (tip) {
+function logTip(tip) {
     console.log('Tip: $' + tip.toFixed(2));
 }
 
 // Print the tax amount to the console
-function logTax (tax) {
+function logTax(tax) {
     console.log('Tax: $' + tax.toFixed(2));
 }
 
 // Print the total amount to the console
-function logTotal (total) {
+function logTotal(total) {
     console.log('--------------\nTotal: $' + total.toFixed(2));
 }
 
 // Print a header to separate the individual bills
-function logDinerBill (name) {
+function logDinerBill(name) {
     console.log('\n-----------The bill for ' + name + ': -----------\n');
 }
 
 // The diners
-var david = new Diner('David');
-var vincent = new Diner('Vincent');
-var max = new Diner('Max');
-var amy = new Diner('Amy');
+let david = new Diner('David');
+let vincent = new Diner('Vincent');
+let max = new Diner('Max');
+let amy = new Diner('Amy');
 
 // Start a new bill
-var bill = new Bill(david, vincent, amy, max);
+let bill = new Bill(david, vincent, amy, max);
 
 // Let's eat
 david.addItem(pasta, soup, beer);
